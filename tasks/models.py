f33cf68a6d0749bd1e0b5a7a67376ec929eb3562
Igor Chapy <misha@mate.acedemy>
2025-04-11 19:31:42 +0300

    Project

---
+++ tasks/models.py
@@ -0,0 +1,18 @@
+
+from django.db import models
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class Task(models.Model):
+    title = models.CharField(max_length=200)
+    description = models.TextField(blank=True)
+    is_done = models.BooleanField(default=False)
+    tags = models.ManyToManyField(Tag, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
